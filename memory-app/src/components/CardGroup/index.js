96a45d7548767c6a67c9f174a9d9dc0d94343d9c
MagusConjurer <cameronisadavis@gmail.com>
2020-07-07 22:32:19 -0600

    Added onClick function to card group to update clicked property of the card

--- memory-app/src/components/CardGroup/index.js
+++ memory-app/src/components/CardGroup/index.js
@@ -12,12 +12,25 @@ class CharacterGroup extends React.Component {
   // Callback to pass click status from card
 
   // Function to shuffle the list of characters
+  handleCardClicked = event => {
+    event.preventDefault();
+
+    const key = parseInt(event.target.getAttribute("data-key"));
+    const index = this.state.characters.findIndex(card => card.id === key);
+    const newArray = this.state.characters.slice();
+    newArray[index].clicked = true;
+    this.setState({
+        characters: newArray
+      });
+  }
 
   render() {
     return (
       <CardGroup>
         {this.state.characters.map(character => <CharacterCard
           key={character.id.toString()}
+          data-key={character.id}
+          onClick={this.handleCardClicked}
           name={character.name}
           image={character.image}
         />)}
--- memory-app/src/components/CharacterCard/index.js
+++ memory-app/src/components/CharacterCard/index.js
@@ -2,18 +2,14 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import "./style.css";
 
-class CharacterCard extends React.Component {
-  fullSRC = process.env.PUBLIC_URL + this.props.image;
+function CharacterCard(props) {
+  var fullSRC = process.env.PUBLIC_URL + props.image;
 
-  // Callback to pass up when clicked
-
-  render() {
-    return (
-      <Card>
-        <Card.Img alt={this.props.name} src={this.fullSRC} />
-      </Card>
-    );
-  };
+  return (
+    <Card>
+      <Card.Img data-key={props["data-key"]} onClick={props.onClick} alt={props.name} src={fullSRC} />
+    </Card>
+  );
 };
 
 export default CharacterCard;
