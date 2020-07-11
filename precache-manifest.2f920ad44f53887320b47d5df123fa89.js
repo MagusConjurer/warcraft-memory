0472f567248faaefdd59f56df6c23f462013e3bd
MagusConjurer <cameronisadavis@gmail.com>
2020-07-11 09:59:04 -0600

    Updates

---
+++ precache-manifest.2f920ad44f53887320b47d5df123fa89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bb6644815b4bb41fef4f5a10279f94",
+    "url": "/warcraft-memory/index.html"
+  },
+  {
+    "revision": "74dc32ba493b4dc956ef",
+    "url": "/warcraft-memory/static/css/main.2eca71a2.chunk.css"
+  },
+  {
+    "revision": "dcefad55d7f98f7a2842",
+    "url": "/warcraft-memory/static/js/2.f49eb620.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/warcraft-memory/static/js/2.f49eb620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74dc32ba493b4dc956ef",
+    "url": "/warcraft-memory/static/js/main.6875b5c2.chunk.js"
+  },
+  {
+    "revision": "c754cdf1e90a8cd08961",
+    "url": "/warcraft-memory/static/js/runtime-main.444f6507.js"
+  }
+]);
